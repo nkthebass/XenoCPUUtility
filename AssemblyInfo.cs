44347b86912b478e8c54ac4dba2fdfac61022274
nkthebass <161380960+nkthebass@users.noreply.github.com>
2026-01-09 10:26:14 -0500

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("Xeno CPU UTI")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyDescription("Xeno CPU utility 1.9.1 - High-Performance Benchmark and Stress Test")]
+[assembly: AssemblyFileVersion("1.9.1.0")]
+[assembly: AssemblyInformationalVersion("1.9.1")]
+[assembly: AssemblyProduct("Xeno CPU utility 1.9.1")]
+[assembly: AssemblyTitle("XenoCPUUtility")]
+[assembly: TargetPlatform("Windows7.0")]
+[assembly: SupportedOSPlatform("Windows7.0")]
+[assembly: AssemblyVersion("1.9.1.0")]
